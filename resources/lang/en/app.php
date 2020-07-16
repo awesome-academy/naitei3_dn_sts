528ce9ce338f1e722798de797ae8b85ff21aaeed
baochanh1801 <duong.bao.chanh@sun-asterisk.com>
2020-07-16 14:24:51 +0700

    init authentication

---
+++ resources/lang/en/app.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'togglenav' => 'Toggle navigation',
+
+];
+
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -19,4 +19,8 @@ return [
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
 
+    'confirmpw' => 'Confirm Password',
+    'confirmarlert' => 'Please confirm your password before continuing.',
+    'resetpw' => 'Reset Password',
+    'sendpwrslink' => 'Send Password Reset Link',
 ];
